217b3dcce4bed2dbb62ea595188d88652366a290
Limseongyun <lsy9699@naver.com>
2020-07-24 15:57:06 +0900

    add Account_Product_spring

---
+++ hy_HD_admin_spring/WebContent/FrameBase/Account_Product.xfdl.js
@@ -0,0 +1,59 @@
+(function()
+{
+    return function()
+    {
+        if (!this._is_form)
+            return;
+        
+        var obj = null;
+        
+        this.on_create = function()
+        {
+            this.set_name("Account_Product");
+            this.set_titletext("New Form");
+            if (Form == this.constructor)
+            {
+                this._setFormPosition(810,700);
+            }
+            
+            // Object(Dataset, ExcelExportObject) Initialize
+
+            
+            // UI Components Initialize
+            obj = new Button("Button00","78","83","212","64",null,null,null,null,null,null,this);
+            obj.set_taborder("0");
+            obj.set_text("계좌상품추가스");
+            this.addChild(obj.name, obj);
+
+            // Layout Functions
+            //-- Default Layout : this
+            obj = new Layout("default","",810,700,this,function(p){});
+            obj.set_mobileorientation("landscape");
+            this.addLayout(obj.name, obj);
+            
+            // BindItem Information
+
+        };
+        
+        this.loadPreloadList = function()
+        {
+
+        };
+        
+        // User Script
+
+        
+        // Regist UI Components Event
+        this.on_initEvent = function()
+        {
+
+        };
+
+        this.loadIncludeScript("Account_Product.xfdl");
+        this.loadPreloadList();
+        
+        // Remove Reference
+        obj = null;
+    };
+}
+)();
